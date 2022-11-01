4d90cdfc9c15b0ca6022688606d909137266f917
Gainutdinov_Daniel_903 <gainutdinovdaniel123@gmail.com>
2022-11-01 18:35:25 +0300

    Урок 12 (Гайнутдинов, 903)

---
+++ Script-12.sql
@@ -0,0 +1,6 @@
+/* 7.1 */
+select substring('Please find the substring in this string', 17, 9);
+/* 7.2 */
+select abs(-25.76823), sign(-25.76823), round(-25.76823, 2);
+/* 7.3 */
+select extract(month from current_date());
